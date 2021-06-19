bbc717acb0ef641acaf91c6207a3f21c332221c4
Ines Mouakher <ines.mouakher@fsegt.utm.tn>
2021-06-19 14:26:34 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   // p = document.getElementById("principal").value;
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100;
+   var year = new Date().getFullYear()+parseInt(years);
+  //Check if amount field is empty
+   document.getElementById("result").innerHTML 
+    =""; 
+  if (principal == "") {
+     document.getElementById("principal").focus();
+      alert("Enter a positive number");
+      return false;
+    }
+    if (principal <=0) {
+        document.getElementById("principal").value = "";
+        document.getElementById("principal").focus();
+     alert("Enter a positive number");
+        return false;
+    }
+  else {
+    document.getElementById("result").innerHTML 
+    = "If you deposit <mark>"+  
+    principal+"</mark>,<br/>"+
+    "at an interest rate of <mark>"+ rate+"%</mark>.<br/>"+
+    "You will receive an amount of <mark>"+ interest+"</mark>,<br/>"+
+    "in the year <mark>"+ year+"</mark>";
+    }
+     return true;
+};
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
